bfc063a34e9060cd12c56cf17ab3ed160487d3b8
Andrew Feeney <webspannerdev@gmail.com>
2016-11-28 09:00:48 +1100

    Stub out controller

---
+++ app/Http/Controllers/JournalController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class JournalController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
