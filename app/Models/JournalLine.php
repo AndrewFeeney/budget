0cc7d1ec123885eca9d58b736e5ee9aafe1248f0
Andrew Feeney <webspannerdev@gmail.com>
2017-03-11 08:17:42 +1100

    Fillable is for schmucks

--- app/Models/JournalLine.php
+++ app/Models/JournalLine.php
@@ -6,17 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class JournalLine extends Model
 {
-    protected $fillable = [
-        'xero_id',
-        'journal_id',
-        'account_xero_id',
-        'account_type',
-        'account_name',
-        'description',
-        'net_amount',
-        'gross_amount',
-        'tax_amount',
-        'tax_type',
-        'tax_name'
-    ];
+    protected $guarded = [];
 }
