5ad26c76ba23b6d9e8bd74e0f53b8e7990635393
Andrew Feeney <webspannerdev@gmail.com>
2017-03-11 08:17:27 +1100

    Add journal factory and journal line factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -26,6 +26,34 @@ $factory->define(App\Models\Account::class, function (Faker\Generator $faker) {
 });
 
 /**
+ * Journal factory
+ **/
+$factory->define(App\Models\Journal::class, function (Faker\Generator $faker) {
+    return [
+        'number' => $faker->randomNumber(5),
+        'xero_id' => $faker->uuid,
+        'date' => $faker->datetime->format('Y-m-d h:m:s'),
+        'source_type' => array_rand(App\Models\ProjectedJournal::SOURCE_TYPES),
+        'created_date_utc' => $faker->dateTime,
+    ];
+});
+
+/**
+ * Journal Line factory
+ **/
+$factory->define(App\Models\JournalLine::class, function (Faker\Generator $faker) {
+
+    $netAmount = $faker->randomFloat(2, 0, 1000);
+
+    return [
+        'xero_id' => $this->faker->uuid,
+        'net_amount' => $netAmount,
+        'gross_amount' => $netAmount * 1.1,
+        'tax_amount' => ($netAmount * 1.1) - $netAmount,
+    ];
+});
+
+/**
  * Projected Journal factory
  **/
 $factory->define(App\Models\ProjectedJournal::class, function (Faker\Generator $faker) {
