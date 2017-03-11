bf48adb5e6a3c1e100caef1aa69f5313f84af087
Andrew Feeney <webspannerdev@gmail.com>
2017-03-12 06:59:34 +1100

    Add Journal test

---
+++ tests/Unit/Models/JournalTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\Models\Journal;
+use Carbon\Carbon;
+
+class JournalTest extends TestCase
+{
+    use CreatesModels;
+    use DatabaseMigrations;
+
+    /** @test */
+    public function amount_method_returns_total_cashflow_represented_by_journal()
+    {
+        $this->createIncome(['amount' => 100]);
+        $journal = $this->getTestObject('journal');
+
+        $this->assertEquals(100, $journal->amount());
+    }
+
+    /** @test */
+    public function between_dates_scope_returns_journals_between_dates()
+    {
+        $includedJournal = $this->createJournal([
+            'date' => '2000-01-01 00:00:00'
+        ]);
+
+        $excludedJournalBefore = $this->createJournal([
+            'date' => '1998-01-01 00:00:00'
+        ]);
+
+        $excludedJournalAfter = $this->createJournal([
+            'date' => '2002-01-01 00:00:00'
+        ]);
+
+        $journals = Journal::betweenDates(
+            Carbon::parse('1999-01-01 00:00:00'), Carbon::parse('2001-01-01 00:00:00')
+        )->get();
+
+        // Assert that the included journal is returned in the query
+        $this->assertTrue($journals->contains(function ($journal) use ($includedJournal) {
+            return $journal->id == $includedJournal->id;
+        }));
+        // Assert that journal before the date is not included
+        $this->assertFalse($journals->contains(function ($journal) use ($excludedJournalBefore) {
+            return $journal->id == $excludedJournalBefore->id;
+        }));
+        // Assert that journal after the date is not included
+        $this->assertFalse($journals->contains(function ($journal) use ($excludedJournalAfter) {
+            return $journal->id == $excludedJournalAfter->id;
+        }));
+    }
+}
