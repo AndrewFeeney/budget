55d6829a24ff13385eb393d8402890a7dca23d9d
Andrew Feeney <webspannerdev@gmail.com>
2017-03-12 06:53:11 +1100

    Create date calculator

---
+++ app/Calculators/DateCalculator.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Calculators;
+
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
+use DatePeriod;
+
+class DateCalculator
+{
+    /**
+     * Returns the current Australian financial year (1st July YYYY - 30th June (YYYY + 1))
+     * as a DatePeriod object
+     *
+     * @return DatePeriod
+     **/
+    public function getThisFinancialYear()
+    {
+        return new DatePeriod(
+            Carbon::now()->month(6)->day(1),
+            CarbonInterval::year(),
+            1
+        );
+    }
+
+    /**
+     * Returns the start date of the curreny Australian financial year as a Carbon object
+     *
+     * @return Carbon
+     **/
+    public function getStartOfThisFinancialYear()
+    {
+        $startOfFinancialYear = Carbon::parse('July 1st Midnight');
+
+        // If the current time is in the first half of the current calendar year
+        if (Carbon::now()->format('i') < 6) {
+            // Return the 1st of July of last year
+            return $startOfFinancialYear->subYear();
+        }
+
+        return $startOfFinancialYear;
+    }
+
+    /**
+     * Returns the start date of the curreny Australian financial year as a Carbon object
+     *
+     * @return Carbon
+     **/
+    public function getEndOfThisFinancialYear()
+    {
+        $startOfFinancialYear = Carbon::parse('June 30th Midnight');
+
+        // If the current time is in the last half of the current calendar year
+        if (Carbon::now()->format('i') > 6) {
+            // Return the 30th June of next year
+            return $startOfFinancialYear->addYear();
+        }
+
+        return $startOfFinancialYear;
+    }
+}
+
---
+++ tests/Unit/Calculators/DateCalculatorTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\Calculators\DateCalculator;
+use Carbon\Carbon;
+
+class DateCalculatorTest extends TestCase
+{
+    /** @test */
+    public function get_start_of_this_financial_year_returns_1st_july_from_year_previous_on_1st_jan()
+    {
+        Carbon::setTestNow(Carbon::parse('1st January 2017 9:00:00am'));
+
+        $calculator = app(DateCalculator::class);
+        $this->assertEquals(
+            '2016-07-01 00:00:00',
+            $calculator->getStartOfThisFinancialYear()->format('Y-m-d H:i:s')
+        );
+    }
+
+    /** @test */
+    public function get_start_of_this_financial_year_returns_1st_july_from_current_year_on_31st_dec()
+    {
+        Carbon::setTestNow(Carbon::parse('31st December 2016 9:00:00am'));
+
+        $calculator = app(DateCalculator::class);
+        $this->assertEquals(
+            '2016-07-01 00:00:00',
+            $calculator->getStartOfThisFinancialYear()->format('Y-m-d H:i:s')
+        );
+    }
+
+    /** @test */
+    public function get_end_of_this_financial_year_returns_30th_june_from_current_year_on_1st_jan()
+    {
+        Carbon::setTestNow(Carbon::parse('1st January 2017 9:00:00am'));
+
+        $calculator = app(DateCalculator::class);
+        $this->assertEquals(
+            '2017-06-30 00:00:00',
+            $calculator->getEndOfThisFinancialYear()->format('Y-m-d H:i:s')
+        );
+    }
+
+    /** @test */
+    public function get_end_of_this_financial_year_returns_30th_june_from_next_year_on_31st_dec()
+    {
+        Carbon::setTestNow(Carbon::parse('31st December 2016 9:00:00am'));
+
+        $calculator = app(DateCalculator::class);
+        $this->assertEquals(
+            '2017-06-30 00:00:00',
+            $calculator->getEndOfThisFinancialYear()->format('Y-m-d H:i:s')
+        );
+    }
+}
